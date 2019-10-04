5782e7a4d066d9b46d6175a58cfa0661c39e6389
Matthias Pröll <proell.matthias@gmail.com>
2019-10-04 13:14:49 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
 if [[ -n $webhook  &&  -n $interval ]]; then
-    python /bot.py $token --daemon --interval $interval --webhook $webhook
+    python /bot.py $token -interval $interval --webhook $webhook
 elif [[ -z $webhook  &&  -n $interval ]]; then
-    python /bot.py $token --daemon --interval $interval
+    python /bot.py $token --interval $interval
 elif [[ -n $webhook  &&  -z $interval ]]; then
-    python /bot.py $token --daemon --webhook $webhook
+    python /bot.py $token --webhook $webhook
 elif [[ -z $webhook  &&  -z $interval ]]; then
-    python /bot.py $token --daemon
+    python /bot.py $token
 fi
 
 while sleep $interval; do
     pgrep "bot.py" && pgrep "python"
     if [ $? == 1 ]; then
         if [[ -n $webhook  &&  -n $interval ]]; then
-            python /bot.py $token --daemon --interval $interval --webhook $webhook
+            python /bot.py $token --interval $interval --webhook $webhook
         elif [[ -z $webhook  &&  -n $interval ]]; then
-            python /bot.py $token --daemon --interval $interval
+            python /bot.py $token --interval $interval
         elif [[ -n $webhook  &&  -z $interval ]]; then
-            python /bot.py $token --daemon --webhook $webhook
+            python /bot.py $token --webhook $webhook
         elif [[ -z $webhook  &&  -z $interval ]]; then
-            python /bot.py $token --daemon
+            python /bot.py $token
         fi
     fi
 done
